0f3b6f5e97b78a755a2ac44e2b2296617ca77ca6
eogud <eogud@DESKTOP-HPISBIT>
2022-01-05 02:14:26 +0900

    수정되는건가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("새로수정된 코드 깃허브에 올릴게용");
+		System.out.println("새로수정된 코드 깃허브에 올릴게용2");
 	}
 }
